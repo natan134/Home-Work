58da34eba0adc1996dfe83e31f39a3833b82e6dd
natan134 <143866137+natan134@users.noreply.github.com>
2023-09-10 21:18:46 +0300

    Update дз(09.09) мкс API.py

--- дз(09.09) мкс API.py
+++ дз(09.09) мкс API.py
@@ -1,22 +1,14 @@
-import urllib.request as urllib2
-import urllib
-import json
-import math 
-url = "http://api.open-notify.org/iss-now.json"
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
-location = result["iss_position"]
-lat = location['latitude']
-lon = location['longitude']
-lat = float(lat)
-lon = float(lon)
-print("\nLatitude: " + str(lat))
-print("\nLongitude: " + str(lon))
-altitude= 253.3204968444*1000
-m2= 5.97600 * math.pow(10,24) 
-m1= 420*1000
-g= 6.6743 * math.pow(10, -11)
-formul = g * (m1 * m2) / math.pow(altitude,2)
-print(formul)
-#Я правда не смог использовать API с чтением высоты(((
+import requests 
+
+response = requests.get("https://api.wheretheiss.at/v1/satellites/25544").json()
+latitude = float(response['latitude'])
+longitude = float(response['longitude'])
+altitude = float(response['altitude'])
+print("Координаты МКС на полдень:", latitude, longitude, altitude)
+r = altitude + 6371000    
+G = 6.67430* (10**-11)
+M = 5.972e24
+m = 420000 * 1000
+attraction = (G * M * m) / (r ** 2)
+print("Притяжение от Земли до МКС на полдень:", attraction)
     
