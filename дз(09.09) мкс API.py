2fed6f5af1d10bcae821c2aa118a4ccd3bf6a8e0
natan134 <143866137+natan134@users.noreply.github.com>
2023-09-16 08:53:18 +0300

    Update дз(09.09) мкс API.py

--- дз(09.09) мкс API.py
+++ дз(09.09) мкс API.py
@@ -6,7 +6,7 @@ longitude = float(response['longitude'])
 altitude = float(response['altitude'])
 print("Координаты МКС на полдень:", latitude, longitude, altitude)
 r = altitude + 6371000    
-G = 6.67430* (10**-11)
+G = 6.67430* pow(10,-11)
 M = 5.972*(10**24)
 m = 420000 * 1000
 attraction = (G * M * m) / (r ** 2)
